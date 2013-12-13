6d3e2c0d7ded7f672bdd4e874ce262282e147e19
Serguei Balanovich <thesergsb@gmail.com>
2013-12-12 19:05:49 -0800

    website updated

--- website/visualization/static/visualization/js/myjs.js
+++ website/visualization/static/visualization/js/myjs.js
@@ -31,9 +31,9 @@ function show(text){
 			document.getElementById("scoreOutput").innerHTML = output;
 			var score = data;
 			document.getElementById("scoreValue").innerHTML = score;
-			output2 = "So how was this done? Well we used our CLF to predict the probability of your post being popular.Then we ran it through our regression model to predict your score! If you'd like to see more, check out the video below or if you'd like to see <b>an even better predictor</b> visit our <a href='http://nbviewer.ipython.org/github/intelligent-dolphins/blob/master/ProcessBook.ipynb'>Process Book</a>"			
+			output2 = "So how was this done? Well we used our CLF of the entire reddit dataset to predict the probability of your post being popular. Then we ran it through our regression model to predict your score! If you'd like to see more, check out the video below or if you'd like to see <b>an even better predictor</b> visit our <a href='http://nbviewer.ipython.org/github/jaysayre/intelligent-dolphins/blob/master/ProcessBook.ipynb'>Process Book</a>"			
 			document.getElementById("scoreOutput2").innerHTML = output2;
-			output3 = '<iframe width="560" height="315" src="//www.youtube.com/embed/gaTtRLYUFtA" frameborder="0" allowfullscreen></iframe>'	
+			output3 = '<iframe id="video" width="100%" height="100%" src="//www.youtube.com/embed/gaTtRLYUFtA" frameborder="0" allowfullscreen></iframe>'	
 			document.getElementById("scoreOutput3").innerHTML = output3;
 			output4 = ''
 			document.getElementById("scoreOutput4").innerHTML = output4;
